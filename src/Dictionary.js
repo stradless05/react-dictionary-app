c848e13380ebed71012f0c1ea13cb67bef657a16
Emily Mahler <102252888+stradless05@users.noreply.github.com>
2022-09-14 11:09:26 +0200

    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
   }
+
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(handleResponse);
+
   function handleKeywordChange(event) {
-    console.log(event.target.value);
     setKeyword(event.target.value);
   }
+
   return (
     <div className="dictionary">
       <form onSubmit={search}>
