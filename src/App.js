b67615e9fe448cc10a43dbe99989da940eb80a89
Emily Mahler <102252888+stradless05@users.noreply.github.com>
2022-09-14 09:59:23 +0200

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello from Emily</header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Emily Mahler</small>
+        </footer>
+      </div>
     </div>
   );
 }
