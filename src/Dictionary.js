ab6fa01a3744808411697899a8fa26d26fdc5e68
Emily Mahler <102252888+stradless05@users.noreply.github.com>
2022-09-18 12:34:56 +0200

    added meaning component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,7 +5,7 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-  let [results, setResults] = useState(null);
+  let [results, setResults] = useState("");
 
   function handleResponse(response) {
     setResults(response.data[0]);
