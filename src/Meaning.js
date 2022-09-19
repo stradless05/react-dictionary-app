7929dce61f08a186b66f4731fb28f1a012f2e08d
Emily Mahler <102252888+stradless05@users.noreply.github.com>
2022-09-19 11:23:18 +0200

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -8,14 +8,11 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              <strong>Definition: {definition.definition}</strong>
-              <br />
-              <strong>Example:</strong>
-              <em>{definition.example}</em>
-              <br />
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
+            <strong>Definition:</strong> {definition.definition}
+            <br />
+            <strong>Example:</strong>
+            <em>{definition.example}</em>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
