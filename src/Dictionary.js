59b1cd131d354879c177bfe39d7e730ee0e8e01e
Emily Mahler <102252888+stradless05@users.noreply.github.com>
2022-09-19 12:12:22 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,12 +8,15 @@ export default function Dictionary() {
   let [results, setResults] = useState("");
 
   function handleResponse(response) {
+    console.log(response);
     setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    console.log(apiUrl);
+
     axios.get(apiUrl).then(handleResponse);
   }
 
