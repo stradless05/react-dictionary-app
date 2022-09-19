5e97b5eee7702bb981c966339292ab3afcaf6918
Emily Mahler <102252888+stradless05@users.noreply.github.com>
2022-09-19 10:35:47 +0200

    created synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -8,9 +9,12 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              {definition.definition}
+              <strong>Definition: {definition.definition}</strong>
               <br />
+              <strong>Example:</strong>
               <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
